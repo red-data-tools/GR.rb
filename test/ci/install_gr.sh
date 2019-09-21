3923bdc1a644b7c7fd79258843e1da08e0d57b70
kojix2 <2xijok@gmail.com>
2019-09-21 22:59:21 +0900

    fix ci

--- test/ci/install_gr.sh
+++ test/ci/install_gr.sh
@@ -6,8 +6,9 @@ CACHE_DIR=$HOME/GR/$GR_VERSION
 
 if [ ! -d "$CACHE_DIR" ]; then
     wget https://github.com/sciapp/gr/releases/download/v$GR_VERSION/gr-$GR_VERSION-Ubuntu-x86_64.tar.gz
-    tar -xf gr-$GR_VERSION-Ubuntu-x86_64.tar.gz
-    mv gr-0.41.2-Ubuntu-x86_64/gr $CACHE_DIR
+    tar -xvf gr-$GR_VERSION-Ubuntu-x86_64.tar.gz
+    mkdir -p $CACHE_DIR
+    mv gr $CACHE_DIR
 else
     echo "GR cached"
 fi
