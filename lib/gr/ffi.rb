a65047dfba9954532229ac4b25b2761b6b2b7641
kojix2 <2xijok@gmail.com>
2019-09-23 04:29:40 +0900

    Add functions needed for the griddata example

--- lib/gr/ffi.rb
+++ lib/gr/ffi.rb
@@ -7,9 +7,21 @@ module GR
     ffi_lib GR.gr_ffi_lib
 
     # https://github.com/sciapp/gr/blob/master/lib/gr/gr.c
-    attach_function :gr_setviewport, %i[double double double double], :void
-    attach_function :gr_axes, %i[double double double double int int double], :void
+    attach_function :gr_updatews, %i[], :void
     attach_function :gr_polyline, %i[int pointer pointer], :void
+    attach_function :gr_polymarker, %i[int pointer pointer], :void
+    attach_function :gr_gridit, %i[int pointer pointer pointer int int pointer pointer pointer], :void
+    attach_function :gr_setmarkertype, %i[int], :void
+    attach_function :gr_setmarkersize, %i[double], :void
+    attach_function :gr_settextfontprec, %i[int int], :void
+    attach_function :gr_setcharheight, %i[double], :void
+    attach_function :gr_settextalign, %i[int int], :void
+    attach_function :gr_setwindow, %i[double double double double], :void
+    attach_function :gr_setviewport, %i[double double double double], :void
+    attach_function :gr_setspace, %i[double double int int], :void
+    attach_function :gr_axes, %i[double double double double int int double], :void
+    attach_function :gr_surface, %i[int int pointer pointer pointer int], :void
+    attach_function :gr_contour, %i[int int int pointer pointer pointer pointer int], :void
     attach_function :gr_version, %i[], :pointer
   end
 end
--- lib/gr/gr.rb
+++ lib/gr/gr.rb
@@ -20,19 +20,66 @@ module GR
 
   # Integrating with Ruby Objects
   class GR < GRBase
-    def polyline(x, y)
-      size = x.size
-      raise if y.size != size
 
-      px = ::FFI::MemoryPointer.new(:double, size)
-      px.write_array_of_double(x)
-      py = ::FFI::MemoryPointer.new(:double, size)
-      py.write_array_of_double(y)
+    def polyline(x, y)
+      px = pointer(:double, x)
+      py = pointer(:double, y)
       super(size, px, py)
+    end
+
+    def polymarker(n, x, y)
+      px = pointer(:double, x)
+      py = pointer(:double, y)
+      super(n, px, py)
+    end
+
+    def gridit(nd, xd, yd, zd, nx, ny)
+      pxd = pointer(:double, xd)
+      pyd = pointer(:double, yd)
+      pzd = pointer(:double, zd)
+      @px  ||= ::FFI::MemoryPointer.new(:double, nx)
+      @py  ||= ::FFI::MemoryPointer.new(:double, ny)
+      @pz  ||= ::FFI::MemoryPointer.new(:double, nx * ny)
+      super(nd, pxd, pyd, pzd, nx, ny, @px, @py, @pz)
+    end
+
+    def surface(nx, ny, option)
+      @px  ||= ::FFI::MemoryPointer.new(:double, nx)
+      @py  ||= ::FFI::MemoryPointer.new(:double, ny)
+      @pz  ||= ::FFI::MemoryPointer.new(:double, nx * ny)
+      super(nx, ny, @px, @py, @pz, option)
+    end
+
+    def contour(nx, ny, h, major_h)
+      nh = h.size
+      ph = pointer(:double, h)
+      @px  ||= ::FFI::MemoryPointer.new(:double, nx)
+      @py  ||= ::FFI::MemoryPointer.new(:double, ny)
+      @pz  ||= ::FFI::MemoryPointer.new(:double, nx * ny)
+      super(nx, ny, nh, @px, @py, ph, @pz, major_h)
     end
 
     def version
       super.read_string
+    end
+
+    private
+
+    def pointer(dtype, data)
+      if narray?(data)
+        data = data.to_a
+      end
+      case dtype
+      when :int, :double
+        pt = ::FFI::MemoryPointer.new(dtype, data.size)
+        pt.send("write_array_of_#{dtype}", data)
+      else
+        raise "Unknown type: #{dtype}"
+      end
+    end
+
+    def narray?(data)
+      defined?(Numo::NArray) && data.is_a?(Numo::NArray)
     end
   end
 end
