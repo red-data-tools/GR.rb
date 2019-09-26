7a71abd357270ae5775f6961903152e38827ff09
kojix2 <2xijok@gmail.com>
2019-09-26 20:45:30 +0900

    Fix cellarray

--- examples/2darray.rb
+++ examples/2darray.rb
@@ -14,7 +14,7 @@ a = 1000 + 255 * (a - amin) / (amax - amin)
 gr.setcolormap(1)
 
 # TODO: report to sciapp
-gr.cellarray(-0.5, 3.5, 3.5, -0.5, 4, 4, 1, 1, 4, 4, a)
+gr.cellarray(-0.5, 3.5, -0.5, 3.5, 4, 4, a)
 
 gr.setviewport(0.825, 0.85, 0.2, 0.9)
 gr.colorbar
--- lib/gr/gr.rb
+++ lib/gr/gr.rb
@@ -48,7 +48,7 @@ module GR
       super(n, x, y)
     end
 
-    def cellarray(xmin, xmax, ymin, ymax, dimx, dimy, _scol, _srow, _ncol, _nrow, color)
+    def cellarray(xmin, xmax, ymin, ymax, dimx, dimy, color)
       pcolor = pointer(:int, color)
       super(xmin, xmax, ymin, ymax, dimx, dimy, 1, 1, dimx, dimy, pcolor)
     end
@@ -73,7 +73,6 @@ module GR
     def contour(px, py, h, pz, major_h)
       nx = length(px, :double)
       ny = length(py, :double)
-      nz = length(pz, :double)
       nh = h.size
       super(nx, ny, nh, px, py, h, pz, major_h)
     end
