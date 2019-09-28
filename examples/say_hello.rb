7d02c6e53643a10a3f3e109cc11ae658915a58bd
kojix2 <2xijok@gmail.com>
2019-09-28 22:54:42 +0900

    Add example say_hello

---
+++ examples/say_hello.rb
@@ -0,0 +1,30 @@
+# https://github.com/jheinen/GR.jl/tree/master/examples
+
+ENV["GKS_ENCODING"] = "utf8"
+
+require 'gr'
+
+hello = {
+  "Chinese" => "你好世界",
+  "Dutch" => "Hallo wereld",
+  "English" => "Hello world",
+  "French" => "Bonjour monde",
+  "German" => "Hallo Welt",
+  "Greek" => "γειά σου κόσμος",
+  "Italian" => "Ciao mondo",
+  "Japanese" => "こんにちは世界",
+  "Korean" => "여보세요 세계",
+  "Portuguese" => "Olá mundo",
+  "Russian" => "Здравствуй, мир",
+  "Spanish" => "Hola mundo"
+}
+
+y = 0.9
+hello.each do |lang, trans|
+  GR.text(0.1, y, lang)
+  GR.text(0.4, y, trans)
+  y -= 0.072
+end
+GR.updatews()
+
+gets
