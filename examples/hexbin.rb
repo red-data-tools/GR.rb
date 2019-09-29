0ee76d515801d5501cdd5b17320273fc2f761904
kojix2 <2xijok@gmail.com>
2019-09-29 17:37:57 +0900

    Add hexbin

---
+++ examples/hexbin.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# https://gr-framework.org/c.html
+require 'gr'
+require 'numo/narray'
+
+x = Numo::DFloat.new(6000).rand_norm
+y = Numo::DFloat.new(6000).rand_norm
+
+GR.clearws
+GR.setviewport(0.1, 0.8, 0.2, 0.9)
+GR.setwindow(-4, 4, -4, 4)
+GR.setcolormap 21
+GR.hexbin(x, y, 40)
+GR.setviewport(0.825, 0.85, 0.2, 0.9)
+GR.colorbar
+GR.updatews
+gets
