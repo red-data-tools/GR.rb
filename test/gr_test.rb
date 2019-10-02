4337a1f41fbedd0557400f1f01f11f2f6e0fdfbe
kojix2 <2xijok@gmail.com>
2019-10-02 11:49:54 +0900

    Add test

--- test/gr_test.rb
+++ test/gr_test.rb
@@ -12,6 +12,15 @@ class GRTest < Minitest::Test
   def test_gr_version
     assert_instance_of String, GR.version
   end
+
+  def test_inqlinetype
+    GR.setlinetype 3
+    assert_equal GR.inqlinetype, 3
+  end
+
+  def teardown
+    GR.clearws
+  end
 end
 
 class GR3Test < Minitest::Test
