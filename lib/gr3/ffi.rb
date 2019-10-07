f03b4206478ff5dbd5cf2c4890e929025b901bc3
kojix2 <2xijok@gmail.com>
2019-10-07 22:48:03 +0900

    Add GR3 ffi methods

--- lib/gr3/ffi.rb
+++ lib/gr3/ffi.rb
@@ -10,6 +10,38 @@ module GR3
 
     extend GRCommons::RegisterMethods
 
+    # Todo: Error
+    attach_function :gr3_init, %i[int], :int
+    attach_function :gr3_terminate, %i[], :void
     attach_function :gr3_clear, %i[], :int
+    attach_function :gr3_usecurrentframebuffer, %i[], :void
+    attach_function :gr3_useframebuffer, %i[uint], :void
+    attach_function :gr3_setbackgroundcolor, %i[float float float float], :void
+    # getbackgroundcolor not implemented
+    # attach_function :gr3_createmesh_nocopy, %i[pinter int pointer pointer pointer], :int
+    # attach_function :gr3_createmesh
+    attach_function :gr3_createindexedmesh_nocopy, %i[pointer int pointer pointer pointer int pointer], :int
+    #attach_function :gr3_createindexedmesh
+    #attach_function :gr3_drawmesh
+    attach_function :gr3_deletemesh, %i[int], :void
+    attach_function :gr3_setlightdirection, %i[float float float], :void
+    attach_function :gr3_cameralookat, %i[float float float float float float float float float], :void
+    attach_function :gr3_setcameraprojectionparameters, %i[float float float], :int
+    attach_function :gr3_getcameraprojectionparameters, %i[pointer pointer pointer], :int
+    attach_function :gr3_drawimage, %i[float float float float int int int], :int
+    attach_function :gr3_setquality, %i[int], :int
+    attach_function :gr3_getimage, %i[int int int pointer], :int
+    attach_function :gr3_export, %i[pointer int int], :int
+    attach_function :gr3_free, %i[pointer], :void
+    #attach_function :gr3_setlogcallback
+    # gr3_geterrorstring
+    # gr3_getrenderpathstring
+    attach_function :gr3_setobjectid, %i[int], :void
+    attach_function :gr3_selectid, %i[int int int int pointer], :int
+    attach_function :gr3_getviewmatrix, %i[pointer], :void
+    #attach_function :gr3_setviewmatrix
+    attach_function :gr3_getprojectiontype, %i[], :int
+    attach_function :gr3_setprojectiontype, %i[int], :void
+
   end
 end
