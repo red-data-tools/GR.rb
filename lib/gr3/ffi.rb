5dea16aed7302ba4dafd11fdc414b2608a5244f9
kojix2 <2xijok@gmail.com>
2019-10-09 00:14:02 +0900

    Autofix by rubocop

--- lib/gr3/ffi.rb
+++ lib/gr3/ffi.rb
@@ -10,7 +10,7 @@ module GR3
 
     extend GRCommons::RegisterMethods
 
-    # Todo: Error
+    # TODO: Error
     attach_function :gr3_init, %i[int], :int
     attach_function :gr3_terminate, %i[], :void
     attach_function :gr3_clear, %i[], :int
@@ -21,8 +21,8 @@ module GR3
     # attach_function :gr3_createmesh_nocopy, %i[pinter int pointer pointer pointer], :int
     # attach_function :gr3_createmesh
     attach_function :gr3_createindexedmesh_nocopy, %i[pointer int pointer pointer pointer int pointer], :int
-    #attach_function :gr3_createindexedmesh
-    #attach_function :gr3_drawmesh
+    # attach_function :gr3_createindexedmesh
+    # attach_function :gr3_drawmesh
     attach_function :gr3_deletemesh, %i[int], :void
     attach_function :gr3_setlightdirection, %i[float float float], :void
     attach_function :gr3_cameralookat, %i[float float float float float float float float float], :void
@@ -33,15 +33,14 @@ module GR3
     attach_function :gr3_getimage, %i[int int int pointer], :int
     attach_function :gr3_export, %i[pointer int int], :int
     attach_function :gr3_free, %i[pointer], :void
-    #attach_function :gr3_setlogcallback
+    # attach_function :gr3_setlogcallback
     # gr3_geterrorstring
     # gr3_getrenderpathstring
     attach_function :gr3_setobjectid, %i[int], :void
     attach_function :gr3_selectid, %i[int int int int pointer], :int
     attach_function :gr3_getviewmatrix, %i[pointer], :void
-    #attach_function :gr3_setviewmatrix
+    # attach_function :gr3_setviewmatrix
     attach_function :gr3_getprojectiontype, %i[], :int
     attach_function :gr3_setprojectiontype, %i[int], :void
-
   end
 end
