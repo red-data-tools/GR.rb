580811231e28daecefadb533776b33630415ed6d
kojix2 <2xijok@gmail.com>
2019-10-10 18:31:17 +0900

    Add show method only when IRuby defined.

--- lib/gr_commons/jupyter_support.rb
+++ lib/gr_commons/jupyter_support.rb
@@ -4,19 +4,21 @@ module GRCommons
   module JupyterSupport
     # For IRuby Notebook
 
-    def self.extended(obj)
-      require 'tempfile'
-      ENV['GKSwstype'] = 'svg'
-      # May be extended to both GR3 and GR
-      ENV['GKS_FILEPATH'] = Tempfile.open(['plot', '.svg']).path
-    end
+    if defined? IRuby
+      def self.extended(_obj)
+        require 'tempfile'
+        ENV['GKSwstype'] = 'svg'
+        # May be extended to both GR3 and GR
+        ENV['GKS_FILEPATH'] = Tempfile.open(['plot', '.svg']).path
+      end
 
-    def show
-      emergencyclosegks
-      sleep 0.5
-      svg = File.read(ENV['GKS_FILEPATH'])
-      IRuby.display(svg, mime: 'image/svg+xml')
-      nil
+      def show
+        emergencyclosegks
+        sleep 0.5
+        svg = File.read(ENV['GKS_FILEPATH'])
+        IRuby.display(svg, mime: 'image/svg+xml')
+        nil
+      end
     end
   end
 end
