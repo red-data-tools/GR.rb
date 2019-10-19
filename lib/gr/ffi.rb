5bb27babec289aadc03b1ff295860692742ea1c4
kojix2 <2xijok@gmail.com>
2019-10-19 20:38:49 +0900

    Sorts and Add the GR3:: FFI functions by gr.h

--- lib/gr/ffi.rb
+++ lib/gr/ffi.rb
@@ -9,21 +9,17 @@ module GR
     begin
       ffi_lib GR.ffi_lib
     rescue LoadError => e
-      raise LoadError, "Could not find GR Framework"
+      raise LoadError, 'Could not find GR Framework'
     end
 
     extend GRCommons::AttachFunction
 
-    # https://github.com/sciapp/gr/blob/master/lib/gr/gr.c
+    # https://github.com/sciapp/gr/blob/master/lib/gr/gr.h 
 
     attach_function :gr_initgr, %i[], :void
-
-    # gks - graphical kernel system
     attach_function :gr_opengks, %i[], :void
     attach_function :gr_closegks, %i[], :void
     attach_function :gr_inqdspsize, %i[pointer pointer pointer pointer], :void
-
-    # ws - workstation
     attach_function :gr_openws, %i[int string int], :void
     attach_function :gr_closews, %i[int], :void
     attach_function :gr_activatews, %i[int], :void
@@ -31,7 +27,6 @@ module GR
     attach_function :gr_configurews, %i[], :void
     attach_function :gr_clearws, %i[], :void
     attach_function :gr_updatews, %i[], :void
-
     attach_function :gr_polyline, %i[int pointer pointer], :void
     attach_function :gr_polymarker, %i[int pointer pointer], :void
     attach_function :gr_text, %i[double double string], :void
@@ -43,8 +38,6 @@ module GR
     attach_function :gr_gdp, %i[int pointer pointer int int pointer], :void
     attach_function :gr_spline, %i[int pointer pointer int int], :void
     attach_function :gr_gridit, %i[int pointer pointer pointer int int pointer pointer pointer], :void
-
-    # inq - inquiry
     attach_function :gr_setlinetype, %i[int], :void
     attach_function :gr_inqlinetype, %i[pointer], :void
     attach_function :gr_setlinewidth, %i[double], :void
@@ -54,10 +47,9 @@ module GR
     attach_function :gr_setmarkertype, %i[int], :void
     attach_function :gr_inqmarkertype, %i[pointer], :void
     attach_function :gr_setmarkersize, %i[double], :void
-    # gr_inqmarkersize is not implemented
+    # gr_inqmarkersize is not implemented.
     attach_function :gr_setmarkercolorind, %i[int], :void
     attach_function :gr_inqmarkercolorind, %i[pointer], :void
-
     attach_function :gr_settextfontprec, %i[int int], :void
     attach_function :gr_setcharexpan, %i[double], :void
     attach_function :gr_setcharspace, %i[double], :void
@@ -66,25 +58,21 @@ module GR
     attach_function :gr_setcharup, %i[double double], :void
     attach_function :gr_settextpath, %i[int], :void
     attach_function :gr_settextalign, %i[int int], :void
-
     attach_function :gr_setfillintstyle, %i[int], :void
     attach_function :gr_inqfillintstyle, %i[pointer], :void
     attach_function :gr_setfillstyle, %i[int], :void
     attach_function :gr_inqfillstyle, %i[pointer], :void
     attach_function :gr_setfillcolorind, %i[int], :void
     attach_function :gr_inqfillcolorind, %i[pointer], :void
-
     attach_function :gr_setcolorrep, %i[int double double double], :void
     attach_function :gr_setscale, %i[int], :int
     attach_function :gr_inqscale, %i[pointer], :void
-
     attach_function :gr_setwindow, %i[double double double double], :void
     attach_function :gr_inqwindow, %i[pointer pointer pointer pointer], :void
     attach_function :gr_setviewport, %i[double double double double], :void
     attach_function :gr_inqviewport, %i[pointer pointer pointer pointer], :void
     attach_function :gr_selntran, %i[int], :void
     attach_function :gr_setclip, %i[int], :void
-
     attach_function :gr_setwswindow, %i[double double double double], :void
     attach_function :gr_setwsviewport, %i[double double double double], :void
     attach_function :gr_createseg, %i[int], :void
@@ -98,22 +86,17 @@ module GR
     attach_function :gr_inqspace, %i[pointer pointer pointer pointer], :void
     attach_function :gr_textext, %i[double double string], :int
     attach_function :gr_inqtextext, %i[double double string pointer pointer], :void
-
-    # attach_function :gr_axeslbl
     attach_function :gr_axes, %i[double double double double int int double], :void
+    # attach_function :gr_axeslbl
     attach_function :gr_grid, %i[double double double double int int], :void
     attach_function :gr_grid3d, %i[double double double double double double int int int], :void
     attach_function :gr_verrorbars, %i[int pointer pointer pointer pointer], :void
     attach_function :gr_herrorbars, %i[int pointer pointer pointer pointer], :void
-
     attach_function :gr_polyline3d, %i[int pointer pointer pointer], :void
     attach_function :gr_polymarker3d, %i[int pointer pointer pointer], :void
     attach_function :gr_axes3d, %i[double double double double double double int int int double], :void
     attach_function :gr_titles3d, %i[string string string], :void
     attach_function :gr_surface, %i[int int pointer pointer pointer int], :void
-    attach_function :gr_trisurface, %i[int pointer pointer pointer], :void
-    attach_function :gr_gradient, %i[int int pointer pointer pointer pointer pointer], :void
-    attach_function :gr_quiver, %i[int int pointer pointer pointer pointer int], :void
     attach_function :gr_contour, %i[int int int pointer pointer pointer pointer int], :void
     attach_function :gr_contourf, %i[int int int pointer pointer pointer pointer int], :void
     attach_function :gr_tricontour, %i[int pointer pointer pointer int pointer], :void
@@ -126,8 +109,8 @@ module GR
     attach_function :gr_inqcolorfromrgb, %i[double double double], :int
     attach_function :gr_hsvtorgb, %i[double double double pointer pointer pointer], :void
     attach_function :gr_tick, %i[double double], :double
-
     attach_function :gr_validaterange, %i[double double], :int
+    attach_function :gr_adjustlimits, %i[pointer pointer], :void
     attach_function :gr_adjustrange, %i[pointer pointer], :void
     attach_function :gr_beginprint, %i[string], :void
     attach_function :gr_beginprintext, %i[string string string string], :void
@@ -143,12 +126,16 @@ module GR
     attach_function :gr_setarrowstyle, %i[int], :void
     attach_function :gr_setarrowsize, %i[double], :void
     attach_function :gr_drawarrow, %i[double double double double], :void
+    # attach_function :gr_readimage, %i[pointer pointer pointer pointer], :int
     attach_function :gr_drawimage, %i[double double double double int int pointer int], :void
+    # attach_function :gr_importgraphics, %i[string], :int
     attach_function :gr_setshadow, %i[double double double], :void
     attach_function :gr_settransparency, %i[double], :void
     attach_function :gr_setcoordxform, %i[pointer], :void
     attach_function :gr_begingraphics, %i[string], :void
     attach_function :gr_endgraphics, %i[], :void
+    # attach_function :gr_getgraphics
+    # attach_function :gr_drawgraphics
     attach_function :gr_mathtex, %i[double double string], :void
     attach_function :gr_inqmathtex, %i[double double string pointer pointer], :void
     attach_function :gr_beginselection, %i[int int], :void
@@ -164,10 +151,41 @@ module GR
     attach_function :gr_selectcontext, %i[int], :void
     attach_function :gr_destroycontext, %i[int], :void
     attach_function :gr_uselinespec, %i[string], :void
-    attach_function :gr_adjustlimits, %i[pointer pointer], :void
-    attach_function :gr_version, %i[], :pointer
-
+    # attach_function :gr_delaunay
     attach_function :gr_reducepoints, %i[int pointer pointer int pointer pointer], :void
+    attach_function :gr_trisurface, %i[int pointer pointer pointer], :void
+    attach_function :gr_gradient, %i[int int pointer pointer pointer pointer pointer], :void
+    attach_function :gr_quiver, %i[int int pointer pointer pointer pointer int], :void
+    # attach_function :gr_interp2
+
+    # gr_newmeta
+    # attach_function :gr_deletemeta
+    # attach_function :gr_finalizemeta
+    # attach_function :gr_meta_args_push
+    # attach_function :gr_meta_args_push_buf
+    # attach_function :gr_meta_args_contains
+    # attach_function :gr_meta_args_clear
+    # attach_function :gr_meta_args_remove
+    # attach_function :gr_meta_get_box
+    # attach_function :gr_openmeta
+    # attach_function :gr_recvmeta
+    # attach_function :gr_sendmeta
+    # attach_function :gr_sendmeta_buf
+    # attach_function :gr_sendmeta_ref
+    # attach_function :gr_sendmeta_args
+    # attach_function :gr_closemeta
+    # attach_function :gr_clearmeta
+    # attach_function :gr_inputmeta
+    # attach_function :gr_mergemeta
+    # attach_function :gr_plotmeta
+    # attach_function :gr_readmeta
+    # attach_function :gr_switchmeta
+    # attach_function :gr_registermeta
+    # attach_function :gr_unregistermeta
+    # attach_function :gr_meta_max_plotid
+    # attach_function :gr_dumpmeta
+    # attach_function :gr_dumpmeta_json
+    attach_function :gr_version, %i[], :pointer
     attach_function :gr_shadepoints, %i[int pointer pointer int int int], :void
     attach_function :gr_shadelines, %i[int pointer pointer int int int], :void
     attach_function :gr_panzoom, %i[double double double double pointer pointer pointer pointer], :void
