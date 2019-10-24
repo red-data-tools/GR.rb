bd1917bee51cd3ab99dea590ea58eb0898a1d9e8
kojix2 <2xijok@gmail.com>
2019-10-24 22:29:04 +0900

    Add yard doc comments for GR

--- lib/gr.rb
+++ lib/gr.rb
@@ -36,58 +36,89 @@ module GR
   # a FFI::MemoryPointer in the GRBase class.
 
   class << self
+
+    # @!method initgr
+    # @!method opengks
+    # @!method closegks
+
+    # inqdspsize
     def inqdspsize
       inquiry %i[double double int int] do |*pts|
         super(*pts)
       end
     end
 
+    # @!method openws
+    # @!method closews
+    # @!method activatews
+    # @!method deactivatews
+    # @!method configurews
+    # @!method clearws
+    # @!method updatews
+
+    # Draw a polyline using the current line attributes, 
+    # starting from the first data point and ending at the last data point.
     def polyline(x, y)
       n = equal_length(x, y)
       super(n, x, y)
     end
 
+    # Draw marker symbols centered at the given data points. 
     def polymarker(x, y)
       n = equal_length(x, y)
       super(n, x, y)
     end
 
+    # @!method text
+
+    # inqtext
     def inqtext(x, y, string)
       inquiry [{ double: 4 }, { double: 4 }] do |tbx, tby|
         super(x, y, string, tbx, tby)
       end
     end
 
+    # Allows you to specify a polygonal shape of an area to be filled.
     def fillarea(x, y)
       n = equal_length(x, y)
       super(n, x, y)
     end
 
+    # Display rasterlike images in a device-independent manner. 
+    # The cell array function partitions a rectangle given by two corner points into DIMX X DIMY cells, 
+    # each of them colored individually by the corresponding color index of the given cell array.
     def cellarray(xmin, xmax, ymin, ymax, dimx, dimy, color)
       super(xmin, xmax, ymin, ymax, dimx, dimy, 1, 1, dimx, dimy, int(color))
     end
 
+    # Display a two dimensional color index array with nonuniform cell sizes.
     def nonuniformcellarray(x, y, dimx, dimy, color)
       raise ArgumentError unless length(x) == dimx + 1 && length(y) == dimy + 1
 
       super(x, y, dimx, dimy, 1, 1, dimx, dimy, int(color))
     end
 
+    # Display a two dimensional color index array mapped to a disk using polar coordinates.
     def polarcellarray(x_org, y_org, phimin, phimax, rmin, rmax, dimphi, dimr, color)
       super(x_org, y_org, phimin, phimax, rmin, rmax, dimphi, dimr, 1, 1, dimphi, dimr, int(color))
     end
 
+    # Generates a generalized drawing primitive (GDP) of the type you specify, 
+    # using specified points and any additional information contained in a data record.
     def gdp(x, y, primid, datrec)
       n = equal_length(x, y)
       ldr = length(datrec, :int)
       super(n, x, y, primid, ldr, datrec)
     end
 
+    # Generate a cubic spline-fit, 
+    # starting from the first data point and ending at the last data point.
     def spline(px, py, m, method)
       n = equal_length(px, py)
       super(n, px, py, m, method)
     end
 
+    # gridit
     def gridit(xd, yd, zd, nx, ny)
       nd = equal_length(xd, yd, zd)
       inquiry [{ double: nx }, { double: ny }, { double: nx * ny }] do |px, py, pz|
@@ -95,92 +126,169 @@ module GR
       end
     end
 
+    # @!method setlinetype
+
+    # inqlinetype
     def inqlinetype
       inquiry_int { |pt| super(pt) }
     end
 
+    # @!method setlinewidth
+
+    # inqlinewidth
     def inqlinewidth
       inquiry_double { |pt| super(pt) }
     end
 
+    # @!method setlinecolorind
+
+    # inqlinecolorind
     def inqlinecolorind
       inquiry_int { |pt| super(pt) }
     end
 
+    # @!method setmarkertype
+
+    # inqmarkertype
     def inqmarkertype
       inquiry_int { |pt| super(pt) }
     end
 
+    # @!method setmarkersize
+
+    # inqmarkersize
     def inqmarkersize
       inquiry_double { |pt| super(pt) }
     end
 
+    # @!method setmarkercolorind
+
+    # inqmarkercolorind
     def inqmarkercolorind
       inquiry_int { |pt| super(pt) }
     end
 
+    # @!method settextfontprec
+    # @!method setcharexpan
+    # @!method setcharspace
+    # @!method settextcolorind
+    # @!method setcharheight
+    # @!method setcharup
+    # @!method settextpath
+    # @!method settextalign
+    # @!method setfillintstyle
+
+    # inqfillintstyle
     def inqfillintstyle
       inquiry_int { |pt| super(pt) }
     end
 
+    # @!method setfillstyle
+
+    # inqfillstyle
     def inqfillstyle
       inquiry_int { |pt| super(pt) }
     end
 
+    # @!method setfillcolorind
+
+    # inqfillcolorind
     def inqfillcolorind
       inquiry_int { |pt| super(pt) }
     end
 
+    # @!method setcolorrep
+    # @!method setscale
+
+    # inqscale
     def inqscale
       inquiry_int { |pt| super(pt) }
     end
 
-    def inqtextext(x, y, string)
-      inquiry [{ double: 4 }, { double: 4 }] do |tbx, tby|
-        super(x, y, string, tbx, tby)
-      end
-    end
+    # @!method setwindow
 
+    # inqwindow
     def inqwindow
       inquiry %i[double double double double] do |*pts|
         super(*pts)
       end
     end
 
+    # @!method setviewport
+
+    # inqviewport
     def inqviewport
       inquiry %i[double double double double] do |*pts|
         super(*pts)
       end
     end
 
+    # @!method selntran
+    # @!method setclip
+    # @!method setwswindow
+    # @!method setwsviewport
+    # @!method createseg
+    # @!method copysegws
+    # @!method redrawsegws
+    # @!method setsegtran
+    # @!method closeseg
+    # @!method emergencyclosegks
+    # @!method updategks
+    # @!method setspace
+
+    # inqspace
     def inqspace
       inquiry %i[double double int int] do |*pts|
         super(*pts)
       end
     end
 
+    # @!method textext
+
+    # inqtextext
+    def inqtextext(x, y, string)
+      inquiry [{ double: 4 }, { double: 4 }] do |tbx, tby|
+        super(x, y, string, tbx, tby)
+      end
+    end
+
+
+    # @!method axes
+
     alias axes2d axes
 
+    # @!method grid
+    # @!method grid3d
+
+    # Draw a standard vertical error bar graph.
     def verrorbars(px, py, e1, e2)
       n = equal_length(px, py, e1, e2)
       super(n, px, py, e1, e2)
     end
 
+    # Draw a standard horizontal error bar graph.
     def herrorbars(px, py, e1, e2)
       n = equal_length(px, py, e1, e2)
       super(n, px, py, e1, e2)
     end
 
+    # Draw a 3D curve using the current line attributes, 
+    # starting from the first data point and ending at the last data point.
     def polyline3d(px, py, pz)
       n = equal_length(px, py, pz)
       super(n, px, py, pz)
     end
 
+    # Draw marker symbols centered at the given 3D data points.
     def polymarker3d(px, py, pz)
       n = equal_length(px, py, pz)
       super(n, px, py, pz)
     end
 
+    # @!method axes3d
+    # @!method titles3d
+
+    # Draw a three-dimensional surface plot for the given data points.
     def surface(px, py, pz, option)
       # TODO: check: Arrays have incorrect length or dimension.
       nx = length(px)
@@ -188,27 +296,9 @@ module GR
       super(nx, ny, px, py, pz, option)
     end
 
-    def trisurface(px, py, pz)
-      n = [length(px), length(py), length(pz)].min
-      super(n, px, py, pz)
-    end
-
-    def gradient(x, y, z)
-      # TODO: check: Arrays have incorrect length or dimension.
-      nx = length(x)
-      ny = length(y)
-      inquiry [{ double: nx * ny }, { double: nx * ny }] do |pu, pv|
-        super(nx, ny, x, y, z, pu, pv)
-      end
-    end
-
-    def quiver(x, y, u, v, color)
-      # TODO: check: Arrays have incorrect length or dimension.
-      nx = length(x)
-      ny = length(y)
-      super(nx, ny, x, y, u, v, (color ? 1 : 0))
-    end
-
+    # Draw contours of a three-dimensional data set 
+    # whose values are specified over a rectangular mesh. 
+    # Contour lines may optionally be labeled.
     def contour(px, py, h, pz, major_h)
       # TODO: check: Arrays have incorrect length or dimension.
       nx = length(px)
@@ -217,6 +307,8 @@ module GR
       super(nx, ny, nh, px, py, h, pz, major_h)
     end
 
+    # Draw filled contours of a three-dimensional data set
+    # whose values are specified over a rectangular mesh.
     def contourf(px, py, h, pz, major_h)
       # TODO: check: Arrays have incorrect length or dimension.
       nx = length(px)
@@ -225,21 +317,27 @@ module GR
       super(nx, ny, nh, px, py, h, pz, major_h)
     end
 
+    # Draw a contour plot for the given triangle mesh.
     def tricontour(x, y, z, levels)
       npoints = length(x) # equal_length ?
       nlevels = length(levels)
       super(npoints, x, y, z, nlevels, levels)
     end
 
+    # hexbin
     def hexbin(x, y, nbins)
       n = length(x)
       super(n, x, y, nbins)
     end
 
+    # @!method setcolormap
+
+    # inqcolormap
     def inqcolormap
       inquiry_int { |pt| super(pt) }
     end
 
+    # Define a linear interpolated colormap by a list of RGB colors.
     def setcolormapfromrgb(r, g, b, positions: nil)
       n = equal_length(r, g, b)
       if positions.nil?
@@ -250,18 +348,33 @@ module GR
       super(n, r, g, b, positions)
     end
 
+    # @!method colorbar
+
+    # inqcolor
     def inqcolor(color)
       inquiry_int do |rgb|
         super(color, rgb)
       end
     end
 
+    # @!method inqcolorfromrgb
+
+    # hsvtorgb
     def hsvtorgb(h, s, v)
       inquiry %i[double double double] do |r, g, b|
         super(h, s, v, r, g, b)
       end
     end
 
+    # @!method tick
+    # @!method validaterange
+    # @!method adjustlimits
+    # @!method adjustrange
+    # @!method beginprint
+    # @!method beginprintext
+    # @!method endprint
+
+    # ndctowc
     def ndctowc(x, y)
       inquiry %i[double double] do |px, py|
         px.write_double x
@@ -270,6 +383,7 @@ module GR
       end
     end
 
+    # wctondc
     def wctondc(x, y)
       inquiry %i[double double] do |px, py|
         px.write_double x
@@ -278,6 +392,7 @@ module GR
       end
     end
 
+    # wc3towc
     def wc3towc(x, y, z)
       inquiry %i[double double double] do |px, py, pz|
         px.write_double x
@@ -287,11 +402,22 @@ module GR
       end
     end
 
+    # @!method drawrect
+    # @!method fillrect
+    # @!method drawarc
+    # @!method fillarc
+
+    # Draw simple and compound outlines consisting of line segments and bezier curves.
     def drawpath(points, codes, fill)
       len = length(codes)
       super(len, points, uint8(codes), fill)
     end
 
+    # @!method setarrowstyle
+    # @!method setarrowsize
+    # @!method drawarrow
+
+    # readimage
     def readimage(path)
       w, h, d = inquiry [:int, :int, :pointer] do |width, height, data|
         # data is a pointer of a pointer
@@ -300,28 +426,90 @@ module GR
       [w, h, d.read_array_of_uint(w * h)]
     end
 
+    # Draw an image into a given rectangular area.
     def drawimage(xmin, xmax, ymin, ymax, width, height, data, model = 0)
       super(xmin, xmax, ymin, ymax, width, height, uint(data), model)
     end
 
+    # @!method importgraphics
+    # @!method setshadow
+    # @!method settransparency
+
+    # Change the coordinate transformation according to the given matrix.
     def setcoordxform(mat)
       raise if mat.size != 6
 
       super(mat)
     end
 
+    # @!method begingraphics
+    # @!method endgraphics
+    # @!method getgraphics
+    # @!method drawgraphics
+    # @!method mathtex
+
+    # inqmathtex
     def inqmathtex(x, y, string)
       inquiry [{ double: 4 }, { double: 4 }] do |tbx, tby|
         super(x, y, string, tbx, tby)
       end
     end
 
+    # @!method beginselection
+    # @!method endselection
+    # @!method moveselection
+    # @!method resizeselection
+
+    # inqbbox
     def inqbbox
       inquiry %i[double double double double] do |*pts|
         super(*pts)
       end
     end
 
+    # @!method precision
+    # @!method setregenflags
+    # @!method inqregenflags
+    # @!method savestate
+    # @!method restorestate
+    # @!method selectcontext
+    # @!method destroycontext
+    # @!method uselinespec
+    
+    # Reduces the number of points of the x and y array.
+    def reducepoints(xd, yd, n)
+      nd = equal_length(xd, yd)
+      inquiry [{ double: n }, { double: n }] do |x, y|
+        # Different from Julia. x, y are initialized zero.
+        super(nd, xd, yd, n, x, y)
+      end
+    end
+
+    # Draw a triangular surface plot for the given data points.
+    def trisurface(px, py, pz)
+      n = [length(px), length(py), length(pz)].min
+      super(n, px, py, pz)
+    end
+
+    # gradient
+    def gradient(x, y, z)
+      # TODO: check: Arrays have incorrect length or dimension.
+      nx = length(x)
+      ny = length(y)
+      inquiry [{ double: nx * ny }, { double: nx * ny }] do |pu, pv|
+        super(nx, ny, x, y, z, pu, pv)
+      end
+    end
+
+    # quiver
+    def quiver(x, y, u, v, color)
+      # TODO: check: Arrays have incorrect length or dimension.
+      nx = length(x)
+      ny = length(y)
+      super(nx, ny, x, y, u, v, (color ? 1 : 0))
+    end
+  
+    # interp2
     def interp2(x, y, z, xq, yq, method, extrapval) # flatten
       nx = length(x)
       ny = length(y)
@@ -333,30 +521,28 @@ module GR
       end
     end
 
+    # version
     def version
       super.read_string
     end
 
-    def reducepoints(xd, yd, n)
-      nd = equal_length(xd, yd)
-      inquiry [{ double: n }, { double: n }] do |x, y|
-        # Different from Julia. x, y are initialized zero.
-        super(nd, xd, yd, n, x, y)
-      end
-    end
-
+    # Display a point set as a aggregated and rasterized image.
     def shadepoints(x, y, dims: [1200, 1200], xform: 1)
       n = length(x)
       w, h = dims
       super(n, x, y, xform, w, h)
     end
 
+    # Display a line set as an aggregated and rasterized image.
     def shadelines(x, y, dims: [1200, 1200], xform: 1)
       n = length(x)
       w, h = dims
       super(n, x, y, xform, w, h)
     end
 
+    # @!method panzoom
+
+    # panzoom
     def panzoom(x, y, zoom)
       inquiry %i[double double double double] do |xmin, xmax, ymin, ymax|
         super(x, y, zoom, zoom, xmin, xmax, ymin, ymax)
