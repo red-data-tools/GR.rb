efbafb71d2ce7a314414578e0f13a26e1d2f44e8
kojix2 <2xijok@gmail.com>
2019-11-08 07:09:04 +0900

    Add aliases to plots

--- lib/gr/plot.rb
+++ lib/gr/plot.rb
@@ -64,60 +64,83 @@ module GR
 
     # Draw one or more line plots.
     def plot_line; end
-
-    def plot_lines; end
+    alias lineplot plot_line
 
     # def plot_line_over oplot_line ?
 
     # Draw one or more step or staircase plots.
     def plot_step; end
+    alias stepplot plot_step
+    alias step plot_step
 
     # Draw one or more scatter plots.
     def plot_scatter; end
+    alias scatterplot plot_scatter
+    alias scatter plot_scatter
 
     # Draw a stem plot.
     def plot_stem; end
+    alias stemplot plot_stem
+    alias stem plot_stem
 
     # Draw a bar plot.
     def plot_bar; end
+    alias barplot plot_bar
+    alias bar plot_bar
 
     def hist; end # should be private?
 
     # Draw a histogram.
     def plot_histgram; end
+    alias histogram plot_histgram
 
     # Draw a polar histogram.
     def plot_polarhistogram; end
+    alias polarhistogram plot_polarhistogram
 
     # Draw a contour plot.
     def plot_contour; end
+    alias contourplot plot_contour
+    # GR.contour is already defined in GR::FFI class.
 
     # Draw a filled contour plot.
     def plot_contourf; end
+    alias contourfplot plot_contour # change name?
+    # GR.contourf is already defined in GR::FFI class.
 
     # Draw a hexagon binning plot.
     def plot_hexbin; end
+    alias hexbinplot plot_hexbin
+    # GR.hexbin is already defined in GR::FFI class.
+    alias jointplot plot_hexbin
 
     # Draw a heatmap.
     def plot_heatmap; end
+    alias heatmap plot_heatmap
 
     def plot_polarheatmap; end
+    alias polarheatmap plot_polarheatmap
 
     # Draw a three-dimensional wireframe plot.
     def plot_wireframe; end
+    alias wireframe plot_wireframe
 
     # Draw a three-dimensional surface plot.
     def plot_surface; end
+    alias surfaceplot plot_surface
 
     def plot_volume; end
+    alias volumeplot plot_volume
 
     # Draw one or more three-dimensional line plots.
     def plot_line3; end
+    alias lineplot3 plot_line3
 
     # Draw one or more three-dimensional scatter plots.
     def plot_scatter3; end
+    alias scatterplot3 plot_scatter3
 
-    # def redraw
+    def redraw; end
 
     def title; end
 
