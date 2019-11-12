0cd0d7cd781c7a2bc71209b24660b16db9cc4320
kojix2 <2xijok@gmail.com>
2019-11-13 08:39:02 +0900

    Update first_plots example

--- examples/first_plots.rb
+++ examples/first_plots.rb
@@ -5,7 +5,7 @@ require 'gr/plot' # will be removed
 
 require 'numo/narray'
 
-x = Numo::DFloat.linspace(0, 4, 401)
+x = Numo::DFloat.linspace(0, 10, 1001)
 y = Numo::NMath.sin(x)
 GR.lineplot(x, y)
 gets
