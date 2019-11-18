fee4e093791c6d747532aa5627174da350d9cd90
kojix2 <2xijok@gmail.com>
2019-11-18 23:37:47 +0900

    Fix examples

--- examples/kws2.rb
+++ examples/kws2.rb
@@ -9,12 +9,14 @@ require 'numo/narray'
 data = File.readlines(File.expand_path('kws2.dat', __dir__))
 data = data[70..-3].join.strip.split(/\s+/).map(&:to_f)
 z = Numo::DFloat.cast(data).reshape(128, 128)
-GR.surfaceplot(z,
-               figsize: [6, 5],
-               rotation: 45, tilt: 78,
-               colormap: 29,
-               xlabel: 'X',
-               ylabel: 'Y',
-               zlabel: 'Counts',
-               accelerate: false)
-gets
+[3, 4].each do |c|
+  GR.surfaceplot(z,
+                 figsize: [6, 5],
+                 rotation: 45, tilt: 78,
+                 colormap: c,
+                 xlabel: 'X',
+                 ylabel: 'Y',
+                 zlabel: 'Counts',
+                 accelerate: false)
+  gets
+end
