6404635c2f3319a35a58c4b1c35f2ee71a094886
kojix2 <2xijok@gmail.com>
2019-11-27 19:53:54 +0900

    Update examples

--- examples/getting_stared.rb
+++ examples/getting_stared.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
-# https://gr-framework.org/c.html
-require 'gr'
+require 'gr/plot'
 
 x = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
 y = [0.3, 0.5, 0.4, 0.2, 0.6, 0.7]
 
-GR.polyline(x, y)
-tick = GR.tick(0, 1)
-GR.axes(tick, tick, 0, 0, 1, 1, -0.001)
-GR.updatews
+GR.lineplot(x, y)
 gets
--- examples/getting_stared3d.rb
+++ examples/getting_stared3d.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 # https://gr-framework.org/c.html
-require 'gr'
+require 'gr/plot'
 
-z = Array.new(100) { |i| i / 100.0 }
-x = Array.new(100) { |i| i * Math.sin(i * Math::PI / 10.0) / 100.0 }
-y = Array.new(100) { |i| i * Math.cos(i * Math::PI / 10.0) / 100.0 }
+2000.times do |i|
+  z = Array.new(i + 1) { |j| j / 100.0 }
+  x = Array.new(i + 1) { |j| j**2 * Math.sin(j * Math::PI / 10.0) / 100.0 }
+  y = Array.new(i + 1) { |j| j**2 * Math.cos(j * Math::PI / 10.0) / 100.0 }
 
-GR.clearws
-GR.setwindow(-1, 1, -1, 1)
-GR.polyline3d(x, y, z)
-tick = GR.tick(0, 1)
-GR.axes3d(tick, tick, tick, 0, 0, 0, 1, 1, 1, -0.001)
-GR.updatews
+  GR.lineplot3(x, y, z, title: "i = #{i}") # method name might be changed in the future. 
+  GR.updatews
+  sleep 0.005
+end
 gets
