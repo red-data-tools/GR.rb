012c5667da57617eba4a386b4400b855636d5755
kojix2 <2xijok@gmail.com>
2019-11-27 13:34:40 +0900

    Fix a example

--- examples/getting_stared3d.rb
+++ examples/getting_stared3d.rb
@@ -3,10 +3,14 @@
 # https://gr-framework.org/c.html
 require 'gr/plot'
 
+x = []
+y = []
+z = []
+
 2000.times do |i|
-  z = Array.new(i + 1) { |j| j / 100.0 }
-  x = Array.new(i + 1) { |j| j**2 * Math.sin(j * Math::PI / 10.0) / 100.0 }
-  y = Array.new(i + 1) { |j| j**2 * Math.cos(j * Math::PI / 10.0) / 100.0 }
+  z <<  i / 100.0
+  x <<  i**2 * Math.sin(i * Math::PI / 10.0) / 100.0
+  y <<  i**2 * Math.cos(i * Math::PI / 10.0) / 100.0
 
   GR.lineplot3(x, y, z, title: "i = #{i}") # method name might be changed in the future.
   GR.updatews
