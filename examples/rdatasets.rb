1a04e78eb6883faa7c3c7f73ee41162391ef23a2
kojix2 <2xijok@gmail.com>
2019-12-03 15:22:22 +0900

    Fix rdatasets.rb in examples

--- examples/rdatasets.rb
+++ examples/rdatasets.rb
@@ -10,16 +10,16 @@ passenger = RDatasets.datasets.AirPassengers
 time = passenger.at(0).to_a
 value = passenger.at(1).to_a
 
-GR.lineplot(time, value,
+GR.plot(time, value,
             title: 'Air Passenger numbers from 1949 to 1961',
             ylabel: "Passenger numbers (1000's)",
             xlabel: 'Date')
 sleep 1.5
 
 volcano = RDatasets.datasets.volcano.to_matrix.to_a
-GR.contourplot(volcano)
+GR.contour(volcano)
 sleep 1.5
-GR.contourfplot(volcano)
+GR.contourf(volcano)
 sleep 1.5
-GR.surfaceplot(volcano)
+GR.surface(volcano)
 sleep 1.5
