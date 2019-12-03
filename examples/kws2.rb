34743a537c381d4f061aaef1eeedc5615cd5c20e
kojix2 <2xijok@gmail.com>
2019-12-03 15:11:06 +0900

    Fix kws2.rb in examples

--- examples/kws2.rb
+++ examples/kws2.rb
@@ -9,13 +9,13 @@ data = File.readlines(File.expand_path('kws2.dat', __dir__))
 data = data[70..-3].join.strip.split(/\s+/).map(&:to_f)
 z = Numo::DFloat.cast(data).reshape(128, 128)
 [3, 4].each do |c|
-  GR.surfaceplot(z,
-                 figsize: [6, 5],
-                 rotation: 45, tilt: 78,
-                 colormap: c,
-                 xlabel: 'X',
-                 ylabel: 'Y',
-                 zlabel: 'Counts',
-                 accelerate: false)
+  GR.surface(z,
+             figsize: [6, 5],
+             rotation: 45, tilt: 78,
+             colormap: c,
+             xlabel: 'X',
+             ylabel: 'Y',
+             zlabel: 'Counts',
+             accelerate: false)
   gets
 end
