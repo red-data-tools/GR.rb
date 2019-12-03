6d528810ea9c93a4248783f69df31d3726d21fe8
kojix2 <2xijok@gmail.com>
2019-12-03 15:08:54 +0900

    Fix contour.rb in example

--- examples/contour.rb
+++ examples/contour.rb
@@ -7,9 +7,9 @@ require 'numo/narray'
 data = File.readlines(File.expand_path('fecr.dat', __dir__)).map(&:to_f)
 
 z = Numo::DFloat.cast(data).reshape(200, 200)
-GR.contourfplot(z)
+GR.contourf(z)
 gets
 
 z = Numo::DFloat.cast(data).reshape(200, 200)
-GR.contourplot(z)
+GR.contour(z, backgroundcolor: 1)
 gets
