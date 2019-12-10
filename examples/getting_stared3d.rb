f4352e79a1b0dade4499b34203456ca222ef6a0b
kojix2 <2xijok@gmail.com>
2019-12-10 20:02:32 +0900

    Fix plot3 example

--- examples/getting_stared3d.rb
+++ examples/getting_stared3d.rb
@@ -12,7 +12,7 @@ z = []
   x <<  i**2 * Math.sin(i * Math::PI / 10.0) / 100.0
   y <<  i**2 * Math.cos(i * Math::PI / 10.0) / 100.0
 
-  GR.lineplot3(x, y, z, title: "i = #{i}") # method name might be changed in the future.
+  GR.plot3(x, y, z, title: "i = #{i}")
   GR.updatews
   sleep 0.005
 end
