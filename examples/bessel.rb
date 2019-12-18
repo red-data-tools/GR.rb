952c218c045a76c054efdc9036dbf1b0e59fad0c
kojix2 <2xijok@gmail.com>
2019-12-18 18:54:26 +0900

    Improve the bessel example

--- examples/bessel.rb
+++ examples/bessel.rb
@@ -2,13 +2,27 @@
 
 # https://github.com/ruby-numo/numo-gnuplot-demo/tree/master/gsl/bessel
 
-# https://github.com/ruby-numo/numo-gsl
+# Use https://github.com/ruby-numo/numo-gsl
 require 'numo/gsl'
 require 'gr/plot'
 
+ENV["GKSwstype"] = "x11"
+
 sz = 150
 x = Numo::DFloat.new(sz).seq(1) / sz * 30
-y = 5.times.map { |n| [x, Numo::GSL::Sf.bessel_Jn(n, x)] }
 
-GR.plot(*y, labels: [*0..4].map(&:to_s))
-gets
+[
+  ['Regular Cylindrical Bessel Functions', :bessel_Jn, 'bessel K', {}],
+  ['Irregular Cylindrical Bessel Functions', :bessel_Yn, 'bessel Y', { ylim: [-2, 1] }],
+  ['Regular Modified Cylindrical Bessel Functions', :bessel_In, 'bessel I', { xlog: true, ylog: true }],
+  ['Irregular Modified Cylindrical Bessel Functions', :bessel_Kn, 'bessel K', { xlog: true, ylog: true }],
+  ['Regular Spherical Bessel Functions', :bessel_jl, 'bessel j', {}],
+  ['Irregular Spherical Bessel Functions', :bessel_yl, 'bessel y', { ylim: [-2, 1] }],
+  ['Regular Modified Spherical Bessel Functions', :bessel_il_scaled, 'bessel i', { xlog: true, ylog: true }],
+  ['Irregular Modified Spherical Bessel Functions', :bessel_kl_scaled, 'bessel k', { xlog: true, ylog: true }]
+].each do |t, method, labelname, opts|
+  y = 6.times.map { |n| [x, Numo::GSL::Sf.send(method, n, x)] }
+  l = 6.times.map { |n| labelname + n.to_s }
+  GR.plot(*y, { title: t, labels: l, xlabel: 'x', ylabel: 'y' }.merge(opts))
+  gets
+end
