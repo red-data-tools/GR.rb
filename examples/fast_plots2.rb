d6359e00316b814aa8b652196574ec6ef344f8fe
kojix2 <2xijok@gmail.com>
2020-04-04 22:04:43 +0900

    Improve fast_plot2 example

--- examples/fast_plots2.rb
+++ examples/fast_plots2.rb
@@ -101,38 +101,10 @@ sleep 1.2
 # GR.scatter3(x, y, z, c)
 # sleep 1.2
 #
-# GR.barplot(%w[a b c d e], [1, 2, 3, 4, 5])
-# sleep 1.2
-#
-# GR.barplot(%w[a b c d e], [1, 2, 3, 4, 5], horizontal: true)
-# sleep 1.2
-#
 # x = DFloat.new(10_000).rand_norm
 # y = DFloat.new(10_000).rand_norm
-# GR.hexbin(x, y)
+# GR.hexbin([x, y], [x + 5, y + 5])
 # sleep 1.2
-#
-# x = 8 * DFloat.new(100).rand - 4
-# y = 8 * DFloat.new(100).rand - 4
-# z = NMath.sin(x) + NMath.cos(y)
-# GR.contour(x, y, z)
-# sleep 1.2
-#
-# _x = DFloat.linspace(-2, 2, 40)
-# _y = DFloat.linspace(0, Math::PI, 20)
-# x = (_x.expand_dims(0) * DFloat.ones(_y.size, 1)).flatten
-# y = (_y.expand_dims(1) * DFloat.ones(1, _x.size)).flatten
-# z = (NMath.sin(x) + NMath.cos(y)).flatten
-# GR.contour(x, y, z)
-# sleep 1.2
-#
-# GR.contourf(x, y, z)
-# sleep 1.2
-#
-# GR.surface(x, y, z)
-# GR3.terminate
-# sleep 1.2
-#
 #
 
 x = 8 * DFloat.new(100).rand - 4
@@ -156,21 +128,11 @@ sleep 1.2
 
 GR.wireframe(*args)
 sleep 1.2
-#
-# z = DFloat.new(50, 50, 50).rand_norm
-# GR.volume(z)
-# sleep 1.2
-# GR.volume(z.sort(axis: 0))
-# sleep 1.2
-# GR.volume(z.sort(axis: 1))
-# sleep 1.2
-# GR.volume(z.sort(axis: 2))
-# sleep 1.2
-#
+
 # n = 1_000_000
 # x = DFloat.new(n).rand_norm
 # y = DFloat.new(n).rand_norm
-# GR.shade(x, y)
-# GR3.terminate
+# GR.shade([x, y])
 # sleep 1.2
-#
+# GR.shade([x, y], [x+5, y+5])
+# sleep 1.2
