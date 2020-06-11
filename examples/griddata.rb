8a1d396e459aa983f298aaf566e8c4d5708debfc
kojix2 <2xijok@gmail.com>
2020-06-11 17:05:46 +0900

    Improved griddata example

--- examples/griddata.rb
+++ examples/griddata.rb
@@ -6,25 +6,23 @@ require 'gr'
 require 'numo/narray'
 
 DFloat = Numo::DFloat
+DFloat.srand
 
 xd = -2 + DFloat.new(100).rand * 4
 yd = -2 + DFloat.new(100).rand * 4
 zd = xd * Numo::NMath.exp(-xd * xd - yd * yd)
 
-h = -0.5 + DFloat.new(20).seq / 19.0
-
 GR.setviewport(0.1, 0.95, 0.1, 0.95)
-GR.setwindow(-2.0, 2.0, -2.0, 2.0)
-GR.setspace(-0.5, 0.5, 0, 90)
-GR.setmarkersize(1.0)
-GR.setmarkertype(-1)
+GR.setwindow(-2, 2, -2, 2)
+GR.setmarkersize(1)
+GR.setmarkertype(GR::MARKERTYPE_SOLID_CIRCLE)
 GR.setcharheight(0.024)
 GR.settextalign(2, 0)
 GR.settextfontprec(3, 0)
 
 x, y, z = GR.gridit(xd, yd, zd, 200, 200)
-GR.surface(x, y, z, 5)
-GR.contour(x, y, h, z, 0)
+h = -0.6.step(0.6, 0.05).to_a
+GR.contourf(x, y, h, z, 2)
 GR.polymarker(xd, yd)
 GR.axes(0.25, 0.25, -2, -2, 2, 2, 0.01)
 
