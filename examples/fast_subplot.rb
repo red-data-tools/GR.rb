e06d500db699f6f652de289b13a9151dd7c3bc20
kojix2 <2xijok@gmail.com>
2020-06-29 10:11:02 +0900

    Fix examples

--- examples/fast_subplot.rb
+++ examples/fast_subplot.rb
@@ -10,14 +10,15 @@ NMath = Numo::NMath
 end
 sleep 1.5
 
-# https://heliosdrm.github.io/GRUtils.jl/latest/#A-basic-example-1
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = x.shuffle
 
-x = DFloat.linspace(0, 10, 500)
-y = NMath.sin(x**2) * NMath.exp(-x)
-GR.plot(x, y, ylim: [-0.5, 0.5],
-              xlabel: 'X', ylabel: 'Y', title: 'Example plot')
-k = GR.hold
-GR.plot([x, -2 * NMath.exp(x)**-1, '--k'], [x, 2 * NMath.exp(x)**-1, '--k'], k)
+GR.scatter x, y, GR.subplot(5, 5, [2, 4], clear: true)
+GR.plot    x, y, GR.subplot(5, 5, [6, 16])
+GR.step    x, y, GR.subplot(5, 5, [10, 20])
+GR.barplot x, y, GR.subplot(5, 5, [7, 19])
+GR.stem    x, y, GR.subplot(5, 5, [22, 24], update: true)
+
 sleep 1.5
 
 ax = DFloat.new(20_000).rand_norm
@@ -31,7 +32,7 @@ x = DFloat.hstack [ax, bx, cx]
 y = DFloat.hstack [ay, by, cy]
 
 GR.shade x, y, subplot: [0.02, 0.2, 0.8, 0.98], clear: true, update: false
-GR.histogram x, subplot: [0.135, 0.89, 0.8, 0.98], clear: false, nbins: 50, update: false
+GR.histogram x, subplot: [0.135, 0.89, 0.8, 0.98], clear: false, nbins: 50, grid: false, update: false
 GR.histogram y, subplot: [0.02, 0.2, 0.0, 0.82], horizontal: true, xflip: true, nbins: 50, grid: false, clear: false, update: false
 GR.hexbin x, y, subplot: [0.12, 1.0, 0.0, 0.82], clear: false, update: true
 sleep 1.5
