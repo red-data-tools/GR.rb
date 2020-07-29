c7754935b71b1986bd9a5d14931af6f4f2f70efb
kojix2 <2xijok@gmail.com>
2020-07-29 10:31:54 +0900

    Add a example of early-stage GRM
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>

---
+++ examples/grm.rb
@@ -0,0 +1,22 @@
+# Original code
+# https://github.com/sciapp/gr/issues/114#issuecomment-619824616
+# Written in Ruby by @kou
+
+require 'grm'
+
+n = 1000
+x = n.times.map { |i| i * 30.0 / n }
+y = n.times.map { |i| Math.cos(x[i]) * x[i] }
+z = n.times.map { |i| Math.sin(x[i]) * x[i] }
+
+args = GRM.args_new
+GRM.args_push(args, 'kind', 's', :const_string, 'plot3')
+GRM.args_push(args, 'x', 'nD', :int, n, :voidp, x.pack('d*'))
+GRM.args_push(args, 'y', 'nD', :int, n, :voidp, y.pack('d*'))
+GRM.args_push(args, 'z', 'nD', :int, n, :voidp, z.pack('d*'))
+
+GRM.plot(args)
+
+gets
+
+GRM.args_delete(args)
