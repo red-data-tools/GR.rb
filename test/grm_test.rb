6d48144814037652974f2255f8320d24a7be01a3
kojix2 <2xijok@gmail.com>
2020-09-16 10:06:03 +0900

    Fix grm_test for older versions of fiddle

--- test/grm_test.rb
+++ test/grm_test.rb
@@ -1,25 +1,29 @@
 # frozen_string_literal: true
 
 require_relative 'test_helper'
-require 'grm'
 
 class GRMTest < Test::Unit::TestCase
-  def setup; end
+  class << self
+    def fiddle_supports_variadic_functions?
+      Fiddle.const_defined?(:VERSION) &&
+        (Gem::Version.new(Fiddle::VERSION) > Gem::Version.new('1.0.0'))
+    end
 
-  def teardown; end
+    def startup
+      require('grm') if fiddle_supports_variadic_functions?
+    end
+  end
+
+  setup
+  def check_fiddle
+    omit_unless GRMTest.fiddle_supports_variadic_functions?
+  end
 
   def test_grm_ffi_lib
-    omit_unless fiddle_supports_variadic_functions?
-    assert_kind_of String, GRM.ffi_lib
+    assert_kind_of(String, GRM.ffi_lib)
   end
 
   def test_version
-    omit_unless fiddle_supports_variadic_functions?
-    assert_kind_of String, GRM::VERSION
-  end
-
-  def fiddle_supports_variadic_functions?
-    Fiddle.const_defined?(:VERSION) &&
-      (Gem::Version.new(Fiddle::VERSION) > Gem::Version.new('1.0.0'))
+    assert_kind_of(String, GRM::VERSION)
   end
 end
