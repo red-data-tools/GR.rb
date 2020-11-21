6c393092cdab7ceb7e42d16f13f2f5356382f36b
kojix2 <2xijok@gmail.com>
2020-11-21 22:43:58 +0900

    Improve the font example

--- examples/fonts.rb
+++ examples/fonts.rb
@@ -1,16 +1,21 @@
+# frozen_string_literal: true
+
 require 'gr/plot'
 
 # This time, Plot module is only used for GR::Plot::FONTS.
-GR.initgr
 
-GR.setcharheight 0.015
+GR.setwsviewport(0, 0.1, 0.0, 0.2)
+GR.setwswindow(0, 0.5, 0, 1)
 
+GR.setcharheight 0.016
+GR.text(0.22, 0.95, 'Fonts')
+
+GR.setcharheight 0.012
 y = 0.9
 GR::Plot::FONTS.each do |name, value|
-  p name
-  next if value == 234 # workaround
   GR.settextfontprec(value, value > 200 ? 3 : 0)
-  GR.text(0.1, y, name)
+  GR.text(0.02, y, value.to_s)
+  GR.text(0.1, y, name.to_s)
   y -= 0.025
 end
 GR.updatews
