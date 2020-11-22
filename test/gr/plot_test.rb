90219d6b7677202a2f3ae1ee66ba2fabdbd41955
kojix2 <2xijok@gmail.com>
2020-11-23 00:49:50 +0900

    Add a test for the Plot class

---
+++ test/gr/plot_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require 'gr/plot'
+
+class PlotTest < Test::Unit::TestCase
+  def setup
+    @plot = GR::Plot.new
+  end
+
+  def test_minmax
+    assert_equal [Float::INFINITY, -Float::INFINITY], @plot.send(:minmax)
+  end
+end
