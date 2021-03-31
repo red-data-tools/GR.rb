63800e27b05e52e97d59888861e52ac1f98d3e27
kojix2 <2xijok@gmail.com>
2021-04-01 08:13:29 +0900

    make pi example bold

--- examples/pi.rb
+++ examples/pi.rb
@@ -6,7 +6,7 @@ require 'numo/narray'
 x = Numo::DComplex.new(314).seq / 100
 w = x * Numo::NMath.exp(-2 * x * 1i)
 
-GR.plot(w.real, w.imag, nil, x.abs, linewidth: 5,
+GR.plot(w.real, w.imag, nil, x.abs, linewidth: 10,
                                     title: "\$f(x) = xe^{-2xi} \\forall x \\in [0,\\pi]\$",
                                     xlabel: "\$\\Re e\\{f(x)\\}\$",
                                     ylabel: "\$\\Im e\\{f(x)\\}\$")
