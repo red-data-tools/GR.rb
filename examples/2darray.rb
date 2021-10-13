7041fb41cb390ba36c64930f400a74ca1cb29f1b
kojix2 <2xijok@gmail.com>
2021-10-13 19:31:23 +0900

    example: Use Ruby Array instead of NArray [skip ci]

--- examples/2darray.rb
+++ examples/2darray.rb
@@ -2,17 +2,19 @@
 
 # https://github.com/sciapp/gr/issues/39#issuecomment-301684973
 require 'gr'
-require 'numo/narray'
 
 GR.setviewport(0.1, 0.8, 0.2, 0.9)
 GR.setwindow(-0.5, 3.5, -0.5, 3.5)
 GR.axes(1, 1, -0.5, -0.5, 1, 1, -0.01)
-a = Numo::UInt32.new(16).seq
-amin, amax = a.minmax
-a = 1000 + 255 * (a - amin) / (amax - amin)
 GR.setcolormap(1)
-GR.cellarray(-0.5, 3.5, -0.5, 3.5, 4, 4, a)
+
+a = Array.new(25) do |i|
+  1000 + 255 * i / 24
+end
+
+GR.cellarray(-0.5, 3.5, -0.5, 3.5, 5, 5, a)
 GR.setviewport(0.825, 0.85, 0.2, 0.9)
 GR.colorbar
 GR.updatews
+
 gets
--- examples/2dpolararray.rb
+++ examples/2dpolararray.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 require 'gr'
-require 'numo/narray'
 
 GR.clearws
 GR.setviewport(0.1, 0.8, 0.2, 0.9)
 GR.setwindow(-2.5, 3.5, -2.5, 3.5)
 GR.axes(1, 1, -0.5, -0.5, 1, 1, -0.01)
-a = Numo::UInt32.new(21).seq
-amin, amax = a.minmax
-a = 1000 + 255 * (a - amin) / (amax - amin)
+
+a = Array.new(21) do |i|
+  1000 + 255 * i / 20
+end
+
 GR.setcolormap(44)
 GR.polarcellarray(0, 0, 0, 360, 0.2, 2, 3, 7, a)
 GR.setviewport(0.825, 0.85, 0.2, 0.9)
