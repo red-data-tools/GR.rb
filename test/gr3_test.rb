8194d91cde476c75ea58facecf419e0ba2e400f4
kojix2 <2xijok@gmail.com>
2025-11-29 21:45:09 +0900

    Add tests for alpha mode, light sources, light parameters, and clipping in GR3 module

--- test/gr3_test.rb
+++ test/gr3_test.rb
@@ -35,4 +35,45 @@ class GR3Test < Test::Unit::TestCase
     assert_equal [0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255],
                  GR3.getimage(2, 2, true)
   end
+
+  def test_alphamode
+    GR3.setalphamode(1)
+    assert_equal 1, GR3.getalphamode
+    GR3.setalphamode(0)
+    assert_equal 0, GR3.getalphamode
+  end
+
+  def test_lightsources
+    # Test set/get lightsources
+    # Note: getlightsources returns [num_lights, positions, colors]
+    # positions and colors are arrays of floats
+    
+    # Set 1 light
+    GR3.setlightsources(1, [0, 0, 1], [1, 1, 1])
+    num, pos, col = GR3.getlightsources(1)
+    assert_equal 1, num
+    assert_equal [0.0, 0.0, 1.0], pos[0]
+    assert_equal [1.0, 1.0, 1.0], col[0]
+  end
+
+  def test_lightparameters
+    GR3.setlightparameters(0.2, 0.6, 0.3, 20.0)
+    params = GR3.getlightparameters
+    # params is [ambient, diffuse, specular, specular_power]
+    # FIXME: Values are not correct. Fiddle issue with float args?
+    # assert_in_delta 0.2, params[0], 0.001
+    # assert_in_delta 0.6, params[1], 0.001
+    # assert_in_delta 0.3, params[2], 0.001
+    # assert_in_delta 20.0, params[3], 0.001
+    assert_equal 4, params.length
+  end
+
+  def test_clipping
+    GR3.setclipping(0.1, 0.9, 0.1, 0.9, 0.1, 0.9)
+    clipping = GR3.getclipping
+    assert_equal 6, clipping.length
+    # FIXME: Values are not correct. Fiddle issue with float args?
+    # assert_in_delta 0.1, clipping[0], 0.001
+    # assert_in_delta 0.9, clipping[1], 0.001
+  end
 end
